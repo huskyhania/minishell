0c46d44857595d0c8931ee35b7f7bb09b12fba51
Hanna Skrzypiec <h.skrzypiec@gmail.com>
2024-12-02 14:57:35 +0200

    main update

--- mainh.c
+++ mainh.c
@@ -6,17 +6,11 @@
 /*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 13:37:20 by hskrzypi          #+#    #+#             */
-/*   Updated: 2024/12/02 14:46:26 by hskrzypi         ###   ########.fr       */
+/*   Updated: 2024/12/02 14:57:02 by hskrzypi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/*# include <errno.h>
-# include <stdio.h>
-# include <sys/wait.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-*/
 
 int main(int argc, char **argv, char **envp)
 {
@@ -24,9 +18,12 @@ int main(int argc, char **argv, char **envp)
 	(void)argv;
 	(void)envp;
 	mini	attributes;
+	attributes.readret = NULL;
 	while (1)
 	{
 		attributes.readret = readline(PROMPT);
+		if (attributes.readret)
+			printf("\n your shitty input was %s\n", attributes.readret);
 	}
 	return (0);
 }
