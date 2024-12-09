6c5fb37de1133157809d68aa003fb051afb0957a
Hanna Skrzypiec <h.skrzypiec@gmail.com>
2024-12-09 12:54:16 +0200

    basic export functionality, unsorted

--- ft_export_builtin.c
+++ ft_export_builtin.c
@@ -6,7 +6,7 @@
 /*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 16:18:06 by hskrzypi          #+#    #+#             */
-/*   Updated: 2024/12/07 19:24:45 by hskrzypi         ###   ########.fr       */
+/*   Updated: 2024/12/09 12:49:33 by hskrzypi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	print_var(char *entry)
 	eq_sign = ft_strchr(entry, '=');
 	if (eq_sign)
 	{
-		write(STDOUT_FILENO, entry, (eq - entry) + 1);
+		write(STDOUT_FILENO, entry, (eq_sign - entry) + 1);
 		printf("\"%s\"\n", eq_sign + 1);
 	}
 	else
@@ -30,23 +30,17 @@ void	print_var(char *entry)
 int	print_export(char **envp)
 {
 	size_t	i;
-	char	**sorted_env;
+	//char	**sorted_env;
 
 	i = 0;
-	sorted_env = env_clone(envp);
-	if (!sorted_env)
-		return (1);
-	env_sort(sorted_env);
-	while (sorted_env[i])
+	while (envp[i])
 	{
-		if (sorted_env[i][0] != '\0')
-			print_var(sorted_env[i]);
+		print_var(envp[i]);
 		i++;
 	}
-	free_array(sorted_env);
 	return (0);
 }
-
+/*
 void	remove_eq_sign(char **envp, char *arg)
 {
 	char	*ptr;
@@ -62,15 +56,21 @@ int	export_single_var(char **envp, char *arg)
 {
 	char	*value;
 	char	*entry_exist;
+	char	*new_entry;
 
 	if (!is_export_valid(arg))
 		return (1);
-	val = ft_strchr(arg, '=');
-	if (val)
-		*val++ = '\0';
+	value = ft_strchr(arg, '=');
+	if (value)
+		*value++ = '\0';
 	entry_exist = find_in_envp(*envp, arg);
 	if (entry_exist)
-		remove_env_var(cmd_array, envp);// potrzebuje calej array?!
+	{
+		if (value)
+		{
+			free(entry_exist);
+			new_entry = ft_strjoin(arg, '=');
+*/
 
 
 int	ft_export(char **cmd_array, char **envp_copy)
@@ -83,14 +83,14 @@ int	ft_export(char **cmd_array, char **envp_copy)
 	//if export is called without any parameter - it should print similar to env
 	if (!cmd_array[i])
 		print_export(envp_copy);
-	while (cmd_array[i] != NULL)
-	{
-		if (export_single_var(envp_copy, cmd_array[i]) == 1)
-		{
-			printf("export error");
-			ret = 1;
-		}
-		i++;
-	}
+	//while (cmd_array[i] != NULL)
+	//{
+	//	if (export_single_var(envp_copy, cmd_array[i]) == 1)
+	//	{
+	//		printf("export error");
+	//		ret = 1;
+	//	}
+	//	i++;
+	//}
 	return (ret);
 }
